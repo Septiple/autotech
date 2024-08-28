9ab810530a19976797439a51165559b18b59c1bb
Alex ten Brink <alextenbrink@gmail.com>
2024-08-28 20:05:23 +0200

    Annotate deque

--- utils/deque.lua
+++ utils/deque.lua
@@ -1,16 +1,34 @@
-local push_left = function(self, value)
+--- @class deque
+--- @field private first integer
+--- @field private last integer
+--- @field private __index deque
+local deque = {}
+deque.__index = deque
+
+---comment
+---@return deque
+function deque.new()
+    local r = {}
+    setmetatable(r, deque)
+
+    r.first = 0
+    r.last = -1
+    return r
+end
+
+function deque:push_left(value)
     local first = self.first - 1
     self.first = first
     self[first] = value
 end
 
-local push_right = function(self, value)
+function deque:push_right(value)
     local last = self.last + 1
     self.last = last
     self[last] = value
 end
 
-local pop_left = function(self)
+function deque:pop_left()
     local first = self.first
     if first > self.last then error("self is empty") end
     local value = self[first]
@@ -19,7 +37,7 @@ local pop_left = function(self)
     return value
 end
 
-local pop_right = function(self)
+function deque:pop_right()
     local last = self.last
     if self.first > last then error("self is empty") end
     local value = self[last]
@@ -28,24 +46,8 @@ local pop_right = function(self)
     return value
 end
 
-
-local is_empty = function(self)
+function deque:is_empty()
     return self.last + 1 == self.first
 end
 
-local methods = {
-    push_right = push_right,
-    push_left = push_left,
-    pop_right = pop_right,
-    pop_left = pop_left,
-    is_empty = is_empty,
-}
-
-local new = function()
-    local r = { first = 0, last = -1 }
-    return setmetatable(r, { __index = methods })
-end
-
-return {
-    new = new,
-}
+return deque
