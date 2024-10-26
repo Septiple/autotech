23efc96a02d3eb74218bac70fbfb1c08446b79e1
Alex ten Brink <alextenbrink@gmail.com>
2024-10-26 12:53:43 +0200

    Removed unused file

--- nodes/start_node.lua
+++
@@ -1,8 +0,0 @@
-local object_node_base = require object_nodes".object_node_base"
-local node_types = require "nodes.node_types"
-
-local start_node = object_node_base:create_object_class("start", node_types.start_node, function(self, nodes)
-    
-end)
-
-return start_node
