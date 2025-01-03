3b01f61bbd5072a02798789da2b046d86343eda3
Alex ten Brink <alextenbrink@gmail.com>
2025-01-03 20:49:13 +0100

    We now get all the way to chemical science

--- functors/entity_functor.lua
+++ functors/entity_functor.lua
@@ -5,39 +5,7 @@ local requirement_types = require "requirement_nodes.requirement_types"
 local item_requirements = require "requirements.item_requirements"
 local entity_requirements = require "requirements.entity_requirements"
 local fluid_requirements = require "requirements.fluid_requirements"
-
-local entity_functor = object_node_functor:new(object_types.entity,
-function (object, requirement_nodes)
-    local entity = object.object
-    requirement_node:add_new_object_dependent_requirement(entity_requirements.instantiate, object, requirement_nodes, object.configuration)
-
-    local minable = entity.minable
-    if minable ~= nil then
-        if minable.required_fluid then
-            requirement_node:add_new_object_dependent_requirement(entity_requirements.required_fluid, object, requirement_nodes, object.configuration)
-        end
-    end
-end,
-function (object, requirement_nodes, object_nodes)
-    local entity = object.object
-    if entity.type == "resource" then
-        object_node_functor:add_typed_requirement_to_object(object, entity.category or "basic-solid", requirement_types.resource_category, requirement_nodes)
-    elseif entity.type == "mining-drill" then
-        object_node_functor:add_fulfiller_for_typed_requirement(object, entity.resource_categories, requirement_types.resource_category, requirement_nodes)
-    elseif entity.type == "offshore-pump" then
-        object_node_functor:add_fulfiller_for_object_requirement(object, entity.fluid, object_types.fluid, fluid_requirements.create, object_nodes)
-    end
-    
-    local minable = entity.minable
-    if minable ~= nil then
-        --self:add_dependency(nodes, node_types.fluid_node, minable.required_fluid, "required fluid", "mine")
-        object_node_functor:add_fulfiller_to_productlike_object(object, minable.results or minable.result, object_nodes)
-        if minable.required_fluid then
-            object_node_functor:reverse_add_fulfiller_for_object_requirement(object, entity_requirements.required_fluid, minable.required_fluid, object_types.fluid, object_nodes)
-        end
-    end
-end)
-return entity_functor
+local technology_requirements = require "requirements.technology_requirements"
 
 -- local is_nonelevated_rail = {
 --     ["curved-rail-a"] = true,
@@ -72,17 +40,48 @@ return entity_functor
 --     ["lightning-attractor"] = true,
 -- }
 
--- local entity_node = object_node_base:create_object_class("entity", node_types.entity_node, function(self, nodes)
---     local entity = self.object
+local entity_functor = object_node_functor:new(object_types.entity,
+function (object, requirement_nodes)
+    local entity = object.object
+    requirement_node:add_new_object_dependent_requirement(entity_requirements.instantiate, object, requirement_nodes, object.configuration)
 
---     self:add_disjunctive_dependent(nodes, node_types.entity_node, entity.remains_when_mined, "remains when mined", entity_verbs.instantiate)
---     self:add_disjunctive_dependency(nodes, node_types.item_node, entity.placeable_by, "placeable by", entity_verbs.instantiate, "item")
---     self:add_disjunctive_dependent(nodes, node_types.item_node, entity.loot, "loot", item_verbs.create, "item")
---     self:add_disjunctive_dependent(nodes, node_types.entity_node, entity.corpse, "corpse", entity_verbs.instantiate)
+    local minable = entity.minable
+    if minable ~= nil then
+        if minable.required_fluid then
+            requirement_node:add_new_object_dependent_requirement(entity_requirements.required_fluid, object, requirement_nodes, object.configuration)
+        end
+    end
+end,
+function (object, requirement_nodes, object_nodes)
+    local entity = object.object
+    if entity.type == "resource" then
+        object_node_functor:add_typed_requirement_to_object(object, entity.category or "basic-solid", requirement_types.resource_category, requirement_nodes)
+    elseif entity.type == "mining-drill" then
+        object_node_functor:add_fulfiller_for_typed_requirement(object, entity.resource_categories, requirement_types.resource_category, requirement_nodes)
+    elseif entity.type == "offshore-pump" then
+        object_node_functor:add_fulfiller_for_object_requirement(object, entity.fluid, object_types.fluid, fluid_requirements.create, object_nodes)
+    end
+    
+    local minable = entity.minable
+    if minable ~= nil then
+        --self:add_dependency(nodes, node_types.fluid_node, minable.required_fluid, "required fluid", "mine")
+        object_node_functor:add_fulfiller_to_productlike_object(object, minable.results or minable.result, object_nodes)
+        if minable.required_fluid then
+            object_node_functor:reverse_add_fulfiller_for_object_requirement(object, entity_requirements.required_fluid, minable.required_fluid, object_types.fluid, object_nodes)
+        end
+    end
+    
+    object_node_functor:add_fulfiller_for_object_requirement(object, entity.remains_when_mined, object_types.entity, entity_requirements.instantiate, object_nodes)
+    if entity.placeable_by then
+        object_node_functor:reverse_add_fulfiller_for_object_requirement(object, entity_requirements.instantiate, entity.placeable_by.item, object_types.item, object_nodes)
+    end
+    object_node_functor:add_fulfiller_for_object_requirement(object, entity.loot, object_types.item, item_requirements.create, object_nodes)
+    object_node_functor:add_fulfiller_for_object_requirement(object, entity.corpse, object_types.entity, entity_requirements.instantiate, object_nodes)
 
---     if entity.autotech_force_require_module_categories then
---         self:add_disjunctive_dependency(nodes, node_types.module_category_node, entity.allowed_module_categories, "allowed module category", module_category_verbs.requires)
---     end
+    -- Support for PyAL-style module requirements
+    if entity.autotech_force_require_module_categories then
+        object_node_functor:add_typed_requirement_to_object(object, entity.allowed_module_categories, requirement_types.module_category, requirement_nodes)
+    end
 
 --     if entity.energy_source then
 --         local energy_source = entity.energy_source
@@ -107,63 +106,81 @@ return entity_functor
 --     if entity.burner then
 --         self:add_disjunctive_dependency(nodes, node_types.fuel_category_node, entity.burner.fuel_categories, "requires fuel", entity_verbs.fuel)
 --     end
---     self:add_disjunctive_dependent(nodes, node_types.recipe_category_node, entity.crafting_categories, "can craft", recipe_category_verbs.instantiate)
+    object_node_functor:add_fulfiller_for_typed_requirement(object, entity.crafting_categories, requirement_types.recipe_category, requirement_nodes)
 
---     local fluid_boxes = entity.fluid_boxes or {}
---     if entity.fluid_box then table.insert(fluid_boxes, entity.fluid_box) end
---     if entity.output_fluid_box then table.insert(fluid_boxes, entity.output_fluid_box) end
---     for _, fluid_box in pairs(fluid_boxes) do
---         if fluid_box.filter then
---             if fluid_box.production_type == "input" then
---                 self:add_dependency(nodes, node_types.fluid_node, fluid_box.filter, "requires fluid input", entity_verbs.required_fluid)
---             elseif fluid_box.production_type == "output" then
---                 self:add_disjunctive_dependent(nodes, node_types.fluid_node, fluid_box.filter, "produces fluid output", fluid_verbs.create)
---             end
---         end
---     end
+    --     local fluid_boxes = entity.fluid_boxes or {}
+    --     if entity.fluid_box then table.insert(fluid_boxes, entity.fluid_box) end
+    --     if entity.output_fluid_box then table.insert(fluid_boxes, entity.output_fluid_box) end
+    --     for _, fluid_box in pairs(fluid_boxes) do
+    --         if fluid_box.filter then
+    --             if fluid_box.production_type == "input" then
+    --                 self:add_dependency(nodes, node_types.fluid_node, fluid_box.filter, "requires fluid input", entity_verbs.required_fluid)
+    --             elseif fluid_box.production_type == "output" then
+    --                 self:add_disjunctive_dependent(nodes, node_types.fluid_node, fluid_box.filter, "produces fluid output", fluid_verbs.create)
+    --             end
+    --         end
+    --     end
+    
+    --     if entity.type == "reactor" or entity.type == "heat-interface" then
+    --         self:add_disjunctive_dependent(nodes, node_types.electricity_node, "heat", "generates heat", electricity_verbs.heat)
+    --     end
 
---     if entity.type == "reactor" or entity.type == "heat-interface" then
---         self:add_disjunctive_dependent(nodes, node_types.electricity_node, "heat", "generates heat", electricity_verbs.heat)
---     end
+    if entity.type == "lab" then
+        local inputs = entity.inputs
+        local input_lookup = {}
+        for _, input in pairs(inputs) do
+            input_lookup[input] = true
+        end
+        for _, technology_node in pairs(object_nodes.nodes[object_types.technology]) do
+            local technology = technology_node.object
+            if technology.unit ~= nil then
+                local matches = true
+                for _, ingredientPair in pairs(technology.unit.ingredients) do
+                    if input_lookup[ingredientPair[1]] ~= true then
+                        matches = false
+                        break
+                    end
+                end
+                if matches then
+                    technology_node.requirements[technology_requirements.researched_with]:add_fulfiller(object)
+                end
+            end
+        end
+    end
 
---     if entity.type == "lab" then
---         self:add_disjunctive_dependent(nodes, node_types.item_node, entity.inputs, "can research with", item_verbs.requires_specific_lab)
---     end
-
---     if entity.type == "plant" then
---         self:add_disjunctive_dependency(nodes, node_types.entity_node, 1, "requires any agri tower prototype", entity_verbs.requires_agri_tower)
---     elseif entity.type == "agricultural-tower" then
---         self:add_disjunctive_dependent(nodes, node_types.entity_node, 1, "can grow", entity_verbs.requires_agri_tower)
---     end
-
---     if entity.type == "rocket-silo" or entity.type == "cargo-bay" or entity.type == "cargo-pod" then
---         self:add_disjunctive_dependency(nodes, node_types.entity_node, 1, "requires any cargo-landing-pad prototype", entity_verbs.requires_cargo_landing_pad)
---     elseif entity.type == "cargo-landing-pad" then
---         self:add_disjunctive_dependent(nodes, node_types.entity_node, 1, "can land rockets on", entity_verbs.requires_cargo_landing_pad)
---     end
-
---     if entity.type == "cargo-pod" then
---         self:add_disjunctive_dependent(nodes, node_types.entity_node, entity.spawned_container, "spawned container", entity_verbs.instantiate)
---     end
-
---     if is_elevated_rail[entity.type] then
---         self:add_dependency(nodes, node_types.entity_node, 1, "requires any rail-support prototype", entity_verbs.requires_rail_supports)
---         self:add_dependency(nodes, node_types.entity_node, 1, "requires any rail-ramp prototype", entity_verbs.requires_rail_ramp)
---     elseif entity.type == "rail-support" then
---         self:add_disjunctive_dependent(nodes, node_types.entity_node, 1, "requires any rail-support prototype", entity_verbs.requires_rail_supports)
---     elseif entity.type == "rail-ramp" then
---         self:add_disjunctive_dependent(nodes, node_types.entity_node, 1, "requires any rail-ramp prototype", entity_verbs.requires_rail_ramp)
---     end
-
---     if is_elevated_rail[entity.type] or is_nonelevated_rail[entity.type] then
---         self:add_disjunctive_dependent(nodes, node_types.entity_node, 1, "requires any rail prototype", entity_verbs.requires_rail)
---     elseif requires_rail_to_build[entity.type] then
---         self:add_dependency(nodes, node_types.entity_node, 1, "requires any rail prototype", entity_verbs.requires_rail)
---     end
-
---     if entity.autoplace then
---         self:add_disjunctive_dependency(nodes, node_types.autoplace_control_node, entity.autoplace.control, "autoplace control", "configure")
---     end
--- end)
-
--- return entity_node
+    --     if entity.type == "plant" then
+    --         self:add_disjunctive_dependency(nodes, node_types.entity_node, 1, "requires any agri tower prototype", entity_verbs.requires_agri_tower)
+    --     elseif entity.type == "agricultural-tower" then
+    --         self:add_disjunctive_dependent(nodes, node_types.entity_node, 1, "can grow", entity_verbs.requires_agri_tower)
+    --     end
+    
+    --     if entity.type == "rocket-silo" or entity.type == "cargo-bay" or entity.type == "cargo-pod" then
+    --         self:add_disjunctive_dependency(nodes, node_types.entity_node, 1, "requires any cargo-landing-pad prototype", entity_verbs.requires_cargo_landing_pad)
+    --     elseif entity.type == "cargo-landing-pad" then
+    --         self:add_disjunctive_dependent(nodes, node_types.entity_node, 1, "can land rockets on", entity_verbs.requires_cargo_landing_pad)
+    --     end
+    
+    --     if entity.type == "cargo-pod" then
+    --         self:add_disjunctive_dependent(nodes, node_types.entity_node, entity.spawned_container, "spawned container", entity_verbs.instantiate)
+    --     end
+    
+    --     if is_elevated_rail[entity.type] then
+    --         self:add_dependency(nodes, node_types.entity_node, 1, "requires any rail-support prototype", entity_verbs.requires_rail_supports)
+    --         self:add_dependency(nodes, node_types.entity_node, 1, "requires any rail-ramp prototype", entity_verbs.requires_rail_ramp)
+    --     elseif entity.type == "rail-support" then
+    --         self:add_disjunctive_dependent(nodes, node_types.entity_node, 1, "requires any rail-support prototype", entity_verbs.requires_rail_supports)
+    --     elseif entity.type == "rail-ramp" then
+    --         self:add_disjunctive_dependent(nodes, node_types.entity_node, 1, "requires any rail-ramp prototype", entity_verbs.requires_rail_ramp)
+    --     end
+    
+    --     if is_elevated_rail[entity.type] or is_nonelevated_rail[entity.type] then
+    --         self:add_disjunctive_dependent(nodes, node_types.entity_node, 1, "requires any rail prototype", entity_verbs.requires_rail)
+    --     elseif requires_rail_to_build[entity.type] then
+    --         self:add_dependency(nodes, node_types.entity_node, 1, "requires any rail prototype", entity_verbs.requires_rail)
+    --     end
+    
+    --     if entity.autoplace then
+    --         self:add_disjunctive_dependency(nodes, node_types.autoplace_control_node, entity.autoplace.control, "autoplace control", "configure")
+    --     end
+end)
+return entity_functor
--- functors/technology_functor.lua
+++ functors/technology_functor.lua
@@ -11,11 +11,10 @@ function (object, requirement_nodes)
     local tech = object.object
 
     --requirement_node:add_new_object_dependent_requirement(technology_requirements.enable, object, requirement_nodes, object.configuration)
-    requirement_node:add_new_object_dependent_requirement(technology_requirements.researched_with, object, requirement_nodes, object.configuration)
-
     requirement_node:add_new_object_dependent_requirement_table(tech.prerequisites, technology_requirements.prerequisite, object, requirement_nodes, object.configuration)
 
     if tech.unit then
+        requirement_node:add_new_object_dependent_requirement(technology_requirements.researched_with, object, requirement_nodes, object.configuration)
         requirement_node:add_new_object_dependent_requirement_table(tech.unit.ingredients, technology_requirements.science_pack, object, requirement_nodes, object.configuration, 1)
     elseif tech.research_trigger then
         requirement_node:add_new_object_dependent_requirement(technology_requirements.trigger, object, requirement_nodes, object.configuration)
@@ -56,26 +55,3 @@ function (object, requirement_nodes, object_nodes)
     end
 end)
 return technology_functor
-
--- local tech = self.object
-
--- if tech.unit then
---     for _, ingredient in pairs(tech.unit.ingredients or {}) do
---         self:add_dependency(nodes, node_types.item_node, ingredient[1], "required science pack", technology_verbs.researched_with)
---     end
--- elseif tech.research_trigger then
---     local trigger = tech.research_trigger
---     if trigger.type == "mine-entity" or trigger.type == "build-entity" then
---         self:add_dependency(nodes, node_types.entity_node, trigger.entity, "researched by mining", technology_verbs.researched_with)
---     elseif trigger.type == "craft-item" then
---         self:add_dependency(nodes, node_types.item_node, trigger.item, "researched by crafting", technology_verbs.researched_with)
---     elseif trigger.type == "craft-fluid" then
---         self:add_dependency(nodes, node_types.fluid_node, trigger.fluid, "researched by crafting", technology_verbs.researched_with)
---     elseif trigger.type == "send-item-to-orbit" then
---         self:add_dependency(nodes, node_types.item_node, trigger.item, "researched by sending to orbit", technology_verbs.researched_with)
---         self:add_dependency(nodes, node_types.entity_node, 1, "requires any cargo-landing-pad prototype", entity_verbs.requires_cargo_landing_pad)
---     elseif trigger.type == "create-space-platform" then
---         -- todo: technically this should be the rocket silo not the landing pad
---         self:add_dependency(nodes, node_types.entity_node, 1, "requires any cargo-landing-pad prototype", entity_verbs.requires_cargo_landing_pad)
---     end
--- end
